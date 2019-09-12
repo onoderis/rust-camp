7d9b17f7b91ec01d2b9454e0d91718815c62b88e
onoderis <onoderis@users.noreply.github.com>
2019-09-13 02:19:55 +0300

    2.2. Tuples

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,35 @@ use core::fmt;
 use std::fmt::{Display, Formatter};
 
 fn main() {
-    formatting();
+    tuples();
 }
+
+// 2.2. Tuples
+struct Matrix(f32, f32, f32, f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        writeln!(f, "( {} {} )", self.0, self.1)?;
+        write!(f, "( {} {} )", self.2, self.3)
+    }
+}
+
+fn tuples() {
+    let matrix = Matrix(
+        1.1, 1.2,
+        2.1, 2.2
+    );
+    println!("Matrix:\n{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
+}
+
+fn transpose(m: Matrix) -> Matrix {
+    Matrix(
+        m.0, m.2,
+        m.1, m.3
+    )
+}
+
 
 // 1.2.3. Formatting
 #[derive(Debug)]
