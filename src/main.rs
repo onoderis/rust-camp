a2c51176726f70318c616c3aa8733961ff9d597e
onoderis <onoderis@users.noreply.github.com>
2019-09-13 01:52:33 +0300

    1.2.3. Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
 #![allow(dead_code)]
 use std::f64::consts::PI;
 use core::fmt;
+use std::fmt::{Display, Formatter};
 
 fn main() {
-    testcase_list();
+    formatting();
 }
+
+// 1.2.3. Formatting
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+fn formatting() {
+    let colors = [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ];
+    for color in colors.iter() {
+        println!("{}", *color);
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "RGB ({}, {}, {})", self.red, self.green, self.blue)?;
+        write!(f, " ")?;
+        write!(f, "0x{:02X}{:02X}{:02X}", self.red, self.green, self.blue)
+    }
+}
+
 
 // 1.2.2.1. Testcase: List
 fn testcase_list() {
